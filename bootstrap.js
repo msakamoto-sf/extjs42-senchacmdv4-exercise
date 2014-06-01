9cdb9644fa881af923e52cc44e6569d448b212b6
Masahiko Sakamoto <sakamoto.gsyc.3s@gmail.com>
2014-06-01 21:46:25 +0900

    apply my-custom-theme to testing build.

--- bootstrap.js
+++ bootstrap.js
@@ -413,6 +413,7 @@ Ext.ClassManager.addNameAlternateMappings({
   ],
   "Ext.grid.RowEditor": [],
   "Ext.grid.RowEditorButtons": [],
+  "Ext.grid.Scroller": [],
   "Ext.grid.View": [],
   "Ext.grid.ViewDropZone": [],
   "Ext.grid.column.Action": [
@@ -1286,6 +1287,7 @@ Ext.ClassManager.addNameAliasMappings({
   "Ext.grid.RowEditorButtons": [
     "widget.roweditorbuttons"
   ],
+  "Ext.grid.Scroller": [],
   "Ext.grid.View": [
     "widget.gridview"
   ],
